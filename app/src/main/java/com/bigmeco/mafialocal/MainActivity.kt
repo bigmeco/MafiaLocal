99d7bb65d7c44f7d18f991ce4225850bcf96ad3f
bigmeco <bigmecoprog@gmail.com>
2020-03-02 10:57:00 +0300

    start project mafia

---
+++ app/src/main/java/com/bigmeco/mafialocal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bigmeco.mafialocal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
