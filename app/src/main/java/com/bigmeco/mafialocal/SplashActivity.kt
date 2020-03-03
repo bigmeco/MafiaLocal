f0c5d848b3d7fe8ce422c90866968e4dede2677a
bigmeco <bigmecoprog@gmail.com>
2020-03-03 13:37:00 +0300

    creating SplashActivity.kt

---
+++ app/src/main/java/com/bigmeco/mafialocal/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.bigmeco.mafialocal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
