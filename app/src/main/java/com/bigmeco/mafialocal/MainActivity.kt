dedf6f38bf2110e2df5e4c78ef8d21c1bf3b580e
bigmeco <bigmecoprog@gmail.com>
2020-07-08 15:44:25 +0300

    add menu

--- app/src/main/java/com/bigmeco/mafialocal/MainActivity.kt
+++ app/src/main/java/com/bigmeco/mafialocal/MainActivity.kt
@@ -7,14 +7,11 @@ import androidx.annotation.FloatRange
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.core.graphics.rotationMatrix
-import androidx.ui.core.Alignment
-import androidx.ui.core.Modifier
-import androidx.ui.core.drawLayer
-import androidx.ui.core.setContent
+import androidx.ui.core.*
 import androidx.ui.foundation.*
-import androidx.ui.graphics.Color
-import androidx.ui.graphics.NativePathEffect
-import androidx.ui.graphics.StrokeJoin
+import androidx.ui.foundation.shape.corner.CircleShape
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.*
 import androidx.ui.graphics.drawscope.Stroke
 import androidx.ui.graphics.drawscope.rotate
 import androidx.ui.graphics.vectormath.rotation
@@ -22,10 +19,12 @@ import androidx.ui.layout.*
 import androidx.ui.material.MaterialTheme
 import androidx.ui.res.colorResource
 import androidx.ui.res.imageResource
+import androidx.ui.res.vectorResource
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.TextUnit
 import androidx.ui.unit.dp
 import kotlinx.coroutines.selects.select
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,32 +40,77 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Host() {
-    HorizontalScroller(isScrollable = true, scrollerPosition = ScrollerPosition(2000f)) {
-        Column(
-            horizontalGravity = Alignment.CenterHorizontally,
 
-            modifier = Modifier.drawLayer(rotationZ = 320f)
+    val tt = ScrollerPosition()
+    Stack(modifier = Modifier.drawBackground(color = Color.Yellow)) {
+
+
+        HorizontalScroller(
+            isScrollable = false,
+            scrollerPosition = tt,
+            modifier = Modifier.wrapContentWidth(Alignment.CenterHorizontally)
         ) {
-            Box(  paddingTop= 100.dp,
-                modifier = Modifier.fillMaxSize().plus(Modifier.weight(1f)).plus(
-                    Modifier.drawBackground(color = Color.Blue)
-                ).plus(Modifier.padding(horizontal = 1000.dp))
+            Column(
+                horizontalGravity = Alignment.CenterHorizontally,
+
+                modifier = Modifier.drawLayer(rotationZ = 320f)
             ) {
-                Text(
-                    text = "Ведущий", fontSize = TextUnit(28)
-                )
+                Box(
+                    gravity = Alignment.BottomCenter,
+                    modifier = Modifier.fillMaxSize().plus(Modifier.weight(1f)).plus(
+                        Modifier.drawBackground(color = Color.Blue)
+                    ).plus(Modifier.padding(horizontal = 1000.dp))
+                        .plus(Modifier.clickable(onClick = {
+                        }))
+                ) {
+
+                    Text(
+                        text = "Ведущий",
+                        fontSize = TextUnit(28),
+                        modifier = Modifier.padding(46.dp)
+                    )
+                }
+
+                Box(
+                    gravity = Alignment.TopCenter,
+                    modifier = Modifier.fillMaxSize().plus(Modifier.weight(1f)).plus(
+                        Modifier.drawBackground(color = Color.Yellow)
+                    ).plus(Modifier.padding(horizontal = 1000.dp))
+                        .plus(Modifier.clickable(onClick = {
+                        }))
+                ) {
+                    Text(
+                        text = "Игрок", fontSize = TextUnit(28),
+                        modifier = Modifier.padding(46.dp)
+                    )
+                }
             }
 
-            Box(   paddingBottom = 100.dp,
-                    modifier =
-                Modifier.fillMaxSize().plus(Modifier.weight(1f)).plus(
-                    Modifier.drawBackground(color = Color.Yellow)
-                ).plus(Modifier.padding(horizontal = 1000.dp))
-            ) {
-                Text(
-                    text = "Игрок", fontSize = TextUnit(28)
-                )
-            }
+        }
+        Box(
+            modifier = Modifier.offset((-80).dp, (-50).dp)
+                .plus(Modifier.drawLayer(rotationZ = 320f))
+                .plus(Modifier.clickable(onClick = {
+                }))
+        ) {
+            Image(
+                vectorResource(id = R.drawable.user), contentScale = ContentScale.Crop,
+                modifier = Modifier.drawShadow(15.dp)
+                    .plus(Modifier.drawBackground(Color.Green))
+                    .plus(Modifier.drawLayer(rotationZ = 40f))
+                    .plus(
+                        Modifier.padding(
+                            top = 80.dp,
+                            start = 100.dp,
+                            end = 120.dp,
+                            bottom = 2.dp
+                        )
+                    )
+                    .plus(Modifier.preferredSize(48.dp))
+
+
+            )
+            tt.scrollTo((tt.maxPosition / 2.0).toFloat())
         }
     }
 
